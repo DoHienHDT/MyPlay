0e108d888228f1319a913c6ac752ddfb1ef7f467
dohien <dohien11091999@gmail.com>
2018-06-06 09:13:35 +0700

    new

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+
+struct Key {
+    var shape: String
+}
+final class person {
+    var name: String = "Me"
+    var key: Key?
+}
+final class Keymaker {
+    weak var customer: Keymaking?
+}
